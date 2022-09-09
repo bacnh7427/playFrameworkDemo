81ef9573a9a21472d2651d2a75b7a44b8d39346c
bacnh7427 <bacnh7427@co-well.com.vn>
2022-09-09 17:55:26 +0700

    Add BooksController

---
+++ app/controllers/BooksController.java
@@ -0,0 +1,38 @@
+package controller;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class BooksController extends Controller{
+
+    // for all books
+    public Result index(){
+        return TODO;
+    }
+
+    // to create book
+    public Result create(){
+        return Todo;
+
+    }
+
+    public Result save(){
+        return Todo;
+    }
+
+    public Result edit(Integer id){
+        return todo;
+    }
+
+    public Result update(){
+        return Todo;
+    }
+
+    public destroy(Integer id){
+        return;
+    }
+
+    // for book details
+    public Result show(Integer id){
+        return
+    }
+}
